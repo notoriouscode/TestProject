fe918b9d7a1c08c9dc4940c7e5a9620c252b5c35
Satyam <eng.satyam89@gmail.com>
2022-09-18 12:23:18 +0530

    add new branch

--- app/src/main/java/com/school/testapplication/MainActivity.kt
+++ app/src/main/java/com/school/testapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("hello","hello1")
+        Log.d("hello","hello2")
     }
 }

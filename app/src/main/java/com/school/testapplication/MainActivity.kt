fc509958f02bf01eb1214c423da2732d8dbe8bee
notoriouscode <satyamsharp89@gmail.com>
2022-09-18 15:32:19 +0530

    Update MainActivity.kt

--- app/src/main/java/com/school/testapplication/MainActivity.kt
+++ app/src/main/java/com/school/testapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("hello","hello3")
+        Log.d("hello","hello4")
     }
 }
